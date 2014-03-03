b61d5213979f5b495eb12a1e6e024621b0b61f13
flpa <flpa.dev@zoho.com>
2014-03-03 22:13:46 +0100

    Finish create draft

--- minidms.lisp
+++ minidms.lisp
@@ -6,23 +6,28 @@
 
 (in-package :com.github.flpa.minidms)
 
-(defvar *docs* nil)
+(defparameter *docs* nil)
 
 (defun create (doc)
-  "Creates a new document and returns it.")
+  "Creates a new document and returns it."
+  (let ((new-doc (finalize-new-doc doc)))
+    (setf *docs* (append *docs* (list new-doc)))
+    new-doc))
 ; needs ID generation, assigns date using get-time-of-day
 
 (defun finalize-new-doc (doc)
   "Finalizes a new document by setting properties not to be set by the user."
   (setf (slot-value doc `id) (new-uuid-string))
-  (setf (slot-value doc `creation-timestamp) (get-universal-time)))
+  (setf (slot-value doc `creation-timestamp) (get-universal-time))
+  doc)
 
 (defun new-uuid-string ()
   (format nil "~a" (get-universal-time)))
 
-((defun search (query)
-  "Searches documents")
-;gets predicate, maxcount, type?, order, return props
+;gets predicate, maxcount, type?, order, return props, wrapping in object prevents breaking api? should this be the api?
+(defun search-docs (predicate)
+  "Searches documents"
+  (remove-if-not predicate *docs*))
 
 (defun search-async ()
   )
